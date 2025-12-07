0ee648c670aec91bbaebb02afa8eb36950228f1f
razeevbro <razeevgrg1@.com>
2025-12-07 12:23:40 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("HI this is day 1 ")
+console.log(12)
+console.log(4+6)
+var a=10;
+var b=20;
+var a=1000;
+console.log(a);
+console.log(b);
+console.error("error detected");
+console.warn("warning detected");
+console.info("information");
+let fname="Razeev";
+console.log(typeof(fname));
+let age=19;
+console.log(typeof(age));
